055c4e2d5d48b9ce7952677d04aec5365cb1dff4
dhebert01 <david.hebertaguilar@dawsoncollege.qc.ca>
2026-06-07 20:36:08 -0400

    create model for HostMetric

---
+++ src/HomeLabMonitoring.Api/Models/HostMetric.cs
@@ -0,0 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace HomeLabMonitoring.Api.Models;
+
+public class HostMetrics{
+    public int Id { get; set; }
+    [Required]
+    public string HostName { get; set; }
+    public double CPU { get; set; }
+    public long MemoryUsed { get; set; }
+    public long MemoryTotal { get; set; }
+    public long NetworkDownload { get; set; }
+    public long NetworkUpload { get; set; }    
+    //uptime in seconds
+    public long Uptime { get; set; }
+
+    public double LoadAverage1m { get; set; }
+    public double LoadAverage5m { get; set; }
+    public double LoadAverage15m { get; set; }
+
+    public DateTime CollectedAt { get; set; }
+}
